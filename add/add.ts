e3b18d3b11087ccddf2c7784603bd66985877bd8
Sparkton <akash1999dubey@gmail.com>
2020-06-22 16:39:59 +0530

    Done

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,21 @@
 function add(param1: number, param2: number): number {
 
+    return param1+param2;
+
 }
 
-// function add2(param1: number[]): number {
+function add2(...param1: number[]): number {
+    let ctr = 0;
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+    param1.forEach((ele) => {
+        ctr +=  ele;
+    });
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+    return ctr;
+}
+console.log(add(1, 2));
+console.log(add(3, 2));
+
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
 
--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,15 @@
-function addTwoDigits(n: any): number {
+function addTwoDigits(n: number): number {
+    let ctr = 0;
+    while(n > 0){
+        ctr += (n%10);
+        n = Math.floor(n / 10);
+    }
 
+    while(ctr > 9)
+        ctr = addTwoDigits(ctr);
+
+    if(ctr <= 9)
+            return ctr;
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(99));
