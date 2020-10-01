5d4f55aab212e16f45cb3d5502ea4211f552c8e0
Sparkton <akash1999dubey@gmail.com>
2020-10-01 17:31:57 +0530

    commit

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,12 @@
 function arrayChange(inputArray: number[]): number {
- 
+    let ctr : number = 0;
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        if(inputArray[i] >= inputArray[i+1]){
+            ctr = ctr + (inputArray[i] - inputArray[i+1] + 1);
+            inputArray[i+1] = inputArray[i]+1;
+        }
+    }
+    return ctr;
 }
 
 console.log(arrayChange([1, 1, 1]));
--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,6 @@
 function arrayConversion(inputArray: number[]): number {
-
+    let isOdd = true;
+    
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
